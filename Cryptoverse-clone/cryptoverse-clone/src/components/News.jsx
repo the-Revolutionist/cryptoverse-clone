69c0505fd563f862d0406112e0e2d15f442e2e1a
Vincnet J Lettau II <vjerome2@live.com>
2021-11-28 19:19:19 -0500

    after completed News.jsx

--- Cryptoverse-clone/cryptoverse-clone/src/components/News.jsx
+++ Cryptoverse-clone/cryptoverse-clone/src/components/News.jsx
@@ -5,16 +5,43 @@ import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
 
 const { Text, Title } = Typography;
 const { Option } = Select;
-
+const demoImage =
+  "http://coinrevolution.com/wp-content/uploads/2020/06/cryptonews.jpg";
 export const News = ({ simplified }) => {
   const { data: cryptoNews } = useGetCryptoNewsQuery({
     newsCatagory: "Cryptocurrency",
-    count: simplified ? 8 : 100,
+    count: simplified ? 6 : 18,
   });
-  console.log(cryptoNews);
+  if (!cryptoNews) {
+    return "Loading...";
+  }
   return (
     <>
-      <div>News</div>
+      <Row gutter={[24, 24]}>
+        {cryptoNews.value.map((news, i) => (
+          <Col key={i} xs={24} sm={12} lg={8}>
+            <Card hoverable className="news-card">
+              <a href={news.url} target="_blank" rel="noreferrer">
+                <div className="news-image-container">
+                  <Title className="news.title" level={4}>
+                    {news.name}
+                  </Title>
+                </div>
+                <img
+                  style={{ width: 100, height: 100 }}
+                  alt="news"
+                  src={news?.image?.thumbnail?.contentUrl || demoImage}
+                />
+                <p>
+                  {news.description > 100
+                    ? `${news.description.substring(0, 50)}...`
+                    : news.description}
+                </p>
+              </a>
+            </Card>
+          </Col>
+        ))}
+      </Row>
     </>
   );
 };
